15fff7e9ce2d33d27c536c4f51d2de617f286a15
Kattenelvis <officialkattenelvis@gmail.com>
2020-07-24 01:47:57 +0200

    so laggy!

--- SocietySimulator.py
+++ SocietySimulator.py
@@ -1,13 +1,14 @@
 import sys, pygame
 pygame.init()
 from collections import namedtuple
-#from math import log, sin
+import pygame.gfxdraw
+from math import ceil #, log, sin
 
 size = width, height = 500, 500
-black = 0, 0, 0
+#black = 0, 0, 0
 
-screen = pygame.display.set_mode(size)
-screen.fill(black)
+screen = pygame.display.set_mode(size, pygame.DOUBLEBUF)
+#screen.fill(0, 0, 0)
 
 pixelarray = pygame.PixelArray(screen)
 
@@ -15,36 +16,56 @@ pixelarray = pygame.PixelArray(screen)
 def f(x, y):
     return x*255/y
 
-for w in range(width):
-    for h in range(height):
-        #Math behind this:
-        #https://www.desmos.com/calculator/vioyppqpcf
-        pixelarray[w][h] = (30, min(f(h+w/10, width)+50, 255), 50)
+def generateBackground():
+    for w in range(width):
+        for h in range(height):
+            #Math behind this:
+            #https://www.desmos.com/calculator/vioyppqpcf
+            pixelarray[w][h] = (30, min(f(h+w/10, width)+50, 255), 50)
         #(j*i)**3*255/(width*height)**3
-from math import ceil
-Tile = namedtuple("Tile", ("rectangle", "color"))
+
+
+Tile = namedtuple("Tile", ("rectangle", "x", "y", "width", "height", "highlighted"))
+
 def generateTiles(xSize, ySize):
-    
-    Map = [[None for i in range(xSize)] for j in range(ySize)]
-    offset = 50
+    Map = [[None for i in range(xSize+1)] for j in range(ySize+1)]
+    offset = 0
     for x in range(xSize):
         for y in range(ySize):
+            #maths for this https://www.desmos.com/calculator/ttdj2hug5v
+            #Note: Please clean this stuff up later.
+
             n = ceil(width/xSize)
             m = ceil(width/ySize)
-            c2 = n*x-n-1+offset
-            y1 = m*y-m-1+offset
+            c2 = n*(x+1)-n-1+offset
+            y1 = m*(y+1)-m-1+offset
 
-            Map[x][y] = pygame.Rect(c2,y1,n,m)
-            pygame.draw.rect(screen, (0,0,0), pygame.Rect(c2,y1,n,m), 1)
-               
-a=10
-generateTiles(a,a)
+            Map[x][y] = Tile(pygame.Rect(c2,y1,n,m), c2, y1, n, m, False)
+
+            pygame.gfxdraw.rectangle(screen, pygame.Rect(c2,y1,n,m), (0,0,0,30))
+    return Map
+
+generateBackground()
+xTiles= 10
+yTiles = 10
+Map = generateTiles(xTiles,yTiles)
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-#
     pygame.display.update()
-    #screen.blit(ball, ballrect)
     pygame.display.flip()
+    
+    mousePos = pygame.mouse.get_pos()
+    generateBackground()
+    
+    for x in range(xTiles):
+        for y in range(yTiles):
+            tile = Map[x][y]
+            pygame.gfxdraw.rectangle(screen, tile.rectangle, (0,0,0,30))
+            
+            if  mousePos[0] > tile.x and mousePos[0] < tile.x + tile.width and mousePos[1] > tile.y and mousePos[1] < tile.y + tile.height:
+                pygame.gfxdraw.box(screen, tile.rectangle, (0,0,0,100))
+        
+
