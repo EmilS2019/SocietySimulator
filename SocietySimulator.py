f537109d109994ed19746502dc46b1cf07e00fbf
Kattenelvis <officialkattenelvis@gmail.com>
2020-07-23 15:53:23 +0200

    mafs

--- SocietySimulator.py
+++ SocietySimulator.py
@@ -21,20 +21,29 @@ for w in range(width):
         #https://www.desmos.com/calculator/vioyppqpcf
         pixelarray[w][h] = (30, min(f(h+w/10, width)+50, 255), 50)
         #(j*i)**3*255/(width*height)**3
-
+from math import ceil
 Tile = namedtuple("Tile", ("rectangle", "color"))
 def generateTiles(xSize, ySize):
     
     Map = [[None for i in range(xSize)] for j in range(ySize)]
-
+    offset = 50
     for x in range(xSize):
         for y in range(ySize):
-            #Generates a single tile
-            TileWidth = int(width/xSize)
-            TileHeight = int(height/ySize)
-            Map[x][y] = Tile(pygame.Rect(TileWidth*x+TileWidth, TileHeight*y+TileHeight, TileWidth, TileHeight), (0,0,0))
-            #Renders the tile to screen
-            pygame.draw.rect(screen, Map[x][y].color, Map[x][y].rectangle, 1)
+            n = ceil(width/xSize)
+            m = ceil(width/ySize)
+            c2 = n*x-n-1+offset
+            y1 = m*y-m-1+offset
+
+            Map[x][y] = pygame.Rect(c2,y1,n,m)
+            pygame.draw.rect(screen, (0,0,0), pygame.Rect(c2,y1,n,m), 1)
+        
+        
+        #     #Generates a single tile
+        #     TileWidth = int(width/xSize)
+        #     TileHeight = int(height/ySize)
+        #     Map[x][y] = Tile(pygame.Rect(TileWidth*x+TileWidth, TileHeight*y+TileHeight, TileWidth, TileHeight), (0,0,0))
+        #     #Renders the tile to screen
+        #     pygame.draw.rect(screen, Map[x][y].color, Map[x][y].rectangle, 1)
             
 a=10
 generateTiles(a,a)
